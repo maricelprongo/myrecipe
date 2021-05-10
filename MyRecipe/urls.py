397d576580975bba941fee9024ca116ec4e91a57
Maricel <Maricel Prongo>
2021-05-10 17:28:21 +0800

    midterm output

--- MyRecipe/urls.py
+++ MyRecipe/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
 	url(r'^$', views.my_recipe, name='my_recipe'),
     url(r'^MyRecipeApp/(\d+)/$', views.ViewRecipe, name='ViewRecipe'),
     url(r'^MyRecipeApp/new$', views.NewRecipe, name='NewRecipe'),
-    url(r'^MyRecipeApp/(\d+)/AddRecipe$', views.AddRecipe, name='AddRecipe'),]
+    url(r'^MyRecipeApp/(\d+)/AddRecipe$', views.AddRecipe, name='AddRecipe'),
+
+ ]
     
 
 
